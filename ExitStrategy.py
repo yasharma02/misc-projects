06c950b8ba86eb1cc0ef703eb672a625906f8d35
Yash Sharma <yasharma02@Yashs-MacBook-Air.local>
2022-01-07 22:46:49 +0530

    adds optimal values in comments

--- ExitStrategy.py
+++ ExitStrategy.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 p0 = 1;
 n0 = 1;
-r = 1;
+r = 0.5;
 t = 0.25;
 c = 1 - t;
 tt = 0;
@@ -46,3 +46,10 @@ plt.plot(xlist, y5list, label = "exit liquidity amount")
 plt.title("return = " + str(r) + ", takeout = " + str(t))
 plt.legend()
 plt.show()
+
+
+# rate of increment 'r' is dependent on the size of project. For large projects the increment step 'r' is small, around 0.5.
+# For small projects the increment step 'r' is large, around 1. 
+# After a certain number of steps (inflection point, around 5) the 'air starts to get thinner', 'things get too hot'.
+# rate of takeout 't' should be such that after the inflection point the 'final after takeout' value starts to decrease.
+# for 'r' = 0.5, 't' = 0.25 (price = 3.5 times, 5 steps) | for 'r' = 1, 't' = 0.2/0.3 (price = 6/5 times, 5/4 steps)
